d78fcd1241699b7292b6e7a972a812e39115c108
stevechan <chanshunli@gmail.com>
2024-04-24 21:33:40 +0800

    resource path no effect

--- src/main/java/com/example/SpringReactBlog/MvcConfig.java
+++ src/main/java/com/example/SpringReactBlog/MvcConfig.java
@@ -3,9 +3,16 @@ package com.example.SpringReactBlog;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**")
+				.addResourceLocations("/public", "classpath:/static/");
+	}
 
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
